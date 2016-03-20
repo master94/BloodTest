cb5d7ad7089ce7d9da994c26b19d30e7e4656bf2
Valery Shevchuk <shevchuk.valery@gmail.com>
2016-03-20 23:07:25 +0200

    Add stream output operator for TestInfo

--- main.cpp
+++ main.cpp
@@ -10,6 +10,25 @@ void showHelp()
     std::cout << "Usage: program <bloodTestImagePath>" << std::endl;
 }
 
+std::ostream &operator<<(std::ostream &out, const TestAnalysis::TestInfo &testInfo)
+{
+    out << testInfo.conc.at(TestAnalysis::SampleType::C1) << std::endl;
+    out << testInfo.conc.at(TestAnalysis::SampleType::C2) << std::endl;
+    out << testInfo.conc.at(TestAnalysis::SampleType::C3) << std::endl;
+    out << testInfo.conc.at(TestAnalysis::SampleType::C4) << std::endl;
+    out << testInfo.conc.at(TestAnalysis::SampleType::C5) << std::endl;
+    out << testInfo.conc.at(TestAnalysis::SampleType::C6) << std::endl;
+    out << testInfo.conc.at(TestAnalysis::SampleType::QC1) << std::endl;
+    out << testInfo.conc.at(TestAnalysis::SampleType::QC2) << std::endl;
+    out << testInfo.conc.at(TestAnalysis::SampleType::Tested) << std::endl;
+
+    out << std::endl;
+
+    out << "QC1 VAR: " << testInfo.qc1Variation << std::endl; 
+    out << "QC2 VAR: " << testInfo.qc2Variation << std::endl; 
+    return out;
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2) {
@@ -23,7 +42,7 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    const auto info = TestAnalysis::detectConcentrations(image);
+    std::cout << TestAnalysis::detectConcentrations(image);
 
     return 0;
 }
